731cf0e7a71824be5115db165a9f681a3e2679bd
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-05-19 22:57:44 +0530

    commit

---
+++ Day1/2nd.py
@@ -0,0 +1,8 @@
+a =33
+b=4
+sum = a+b
+print(sum)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
---
+++ Day2/conditionals.py
@@ -0,0 +1,8 @@
+age = int(input('Enter you age'))
+if (age < 18):
+    print("you are not elegible to vote ")
+elif (age >= 18):
+    print("You can vote")
+else:
+    print("bhaad me jaao tum ")
+    
