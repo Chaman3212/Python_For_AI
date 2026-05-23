c09562d2a420ec12e401a9f7dfbd3496712f4a3a
Chaman3212 <chaman.gla_cs24@gla.ac.in>
2026-05-23 22:19:29 +0530

    commit

--- Day6/studentClass.py
+++ Day6/studentClass.py
@@ -1,6 +1,13 @@
 class student:
-    subject="python"
+    def __init__(self,name,cgpa):
+        self.name= name
+        self.cgpa = cgpa
+    def get_cgpa(self):
+        return self.cgpa
 
 
-stu1= student()
-print(stu1)
+stu1= student("Rahul",9.2)
+stu2= student("Rajiv",7.6)
+stu3= student("Rakesh",8.9)
+
+print(f"the name of the student is {stu1.name} and the cgpa is {stu1.cgpa}")
